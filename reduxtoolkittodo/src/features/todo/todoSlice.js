4d8ffc1f8046db851ece942600e632df39ef39bf
krish <sharmageeta847@gmail.com>
2023-10-23 14:39:38 +0530

    appwrite initalize

--- reduxtoolkittodo/src/features/todo/todoSlice.js
+++ reduxtoolkittodo/src/features/todo/todoSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
-import { act } from "@testing-library/react";
+
 
 const initialState = {
     todos: [{id:1, text:"Hello world"}]
@@ -19,8 +19,17 @@ export const todoSlice = createSlice({
         removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         },
+        // updateTodo: (state, action) => {
+        //     state.todos = state.todos.map((todo) => todo.id === id? todo : action.payload)
+        // }
+        updateTodo: (state, action) => {
+            // const { id, text } = action.payload;
+            state.todos = state.todos.map((todo) => (todo.id === id ? { ...todo, text } : todo));
+        }
+        
+        
      }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
 export default todoSlice.reducer
